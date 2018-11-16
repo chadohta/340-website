468cd649f03a9a73e9dbab617c4ca72f3748baeb
Chad Ohta <chadohta@uw.edu>
2018-11-16 13:50:59 -0800

    Updates

---
+++ precache-manifest.6aff916bf82bd94501e4d151712ba21c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c452aace10ff8fcb169",
+    "url": "/chadohta/340-website/static/js/runtime~main.9c452aac.js"
+  },
+  {
+    "revision": "6546450b2202228f74b2",
+    "url": "/chadohta/340-website/static/js/main.6546450b.chunk.js"
+  },
+  {
+    "revision": "bf977317a6f55395bbe1",
+    "url": "/chadohta/340-website/static/js/1.bf977317.chunk.js"
+  },
+  {
+    "revision": "6546450b2202228f74b2",
+    "url": "/chadohta/340-website/static/css/main.5c692dbb.chunk.css"
+  },
+  {
+    "revision": "bf977317a6f55395bbe1",
+    "url": "/chadohta/340-website/static/css/1.229b8d6f.chunk.css"
+  },
+  {
+    "revision": "49e196344ecdd4d899ed345cd0cd2300",
+    "url": "/chadohta/340-website/index.html"
+  }
+];
