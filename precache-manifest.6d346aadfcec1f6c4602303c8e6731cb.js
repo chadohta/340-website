c54c9444a7328d126c037d81d2b4195950462ed8
Chad Ohta <chadohta@uw.edu>
2018-11-16 17:03:46 -0800

    Updates

---
+++ precache-manifest.6d346aadfcec1f6c4602303c8e6731cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1eb0a2b6737603c3a17",
+    "url": "/340-website/static/js/runtime~main.c1eb0a2b.js"
+  },
+  {
+    "revision": "9cb274badb3ef04aba50",
+    "url": "/340-website/static/js/main.9cb274ba.chunk.js"
+  },
+  {
+    "revision": "bf977317a6f55395bbe1",
+    "url": "/340-website/static/js/1.bf977317.chunk.js"
+  },
+  {
+    "revision": "9cb274badb3ef04aba50",
+    "url": "/340-website/static/css/main.5c692dbb.chunk.css"
+  },
+  {
+    "revision": "bf977317a6f55395bbe1",
+    "url": "/340-website/static/css/1.229b8d6f.chunk.css"
+  },
+  {
+    "revision": "54126da62f9e67d4513c7906fff0e690",
+    "url": "/340-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/chadohta/340-website/precache-manifest.6aff916bf82bd94501e4d151712ba21c.js"
+  "/340-website/precache-manifest.6d346aadfcec1f6c4602303c8e6731cb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/chadohta/340-website/index.html", {
+workbox.routing.registerNavigationRoute("/340-website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
